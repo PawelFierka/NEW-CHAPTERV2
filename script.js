ea1f807af88b3a3d41e2018de8c95b2757364dbf
PawelFierka <pjfierka@gmail.com>
2025-03-13 12:44:14 +0100

    -m

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function addZero() {
-    document.querySelectorAll(".price").forEach(node => node.innerHTML = 200 + ",00")
-  }
+  document
+    .querySelectorAll(".price")
+    .forEach((node) => (node.innerHTML = 200 + ",00"));
+}
 
 addZero();
+
+const button = document.getElementById("printer");
+
+button.addEventListener("click", function () {
+  let toPrint = document.getElementById("prices");
+  html2pdf(toPrint);
+});
